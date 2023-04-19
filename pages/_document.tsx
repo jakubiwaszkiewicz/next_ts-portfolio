e8fb16b16a64d9138e3df8612adfd6acdb237a0a
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-04-19 18:32:13 +0200

    add: git init; changed: clearing template

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pl">
+      <Head>
+        <title>iw4sz's Portfolio</title>
+        <meta property="og:title" content="iw4sz's Portfolio" key="title" />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-red-500">Hello world!</h1>
+    </main>
+
+    // {Header}
+    // {Hero}
+    // {About}
+    // {Skills}
+    // {Projects}
+    // {Contact Me}
+  )
+}
